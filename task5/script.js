a2315a26f414973f5254ff17647c3603e73a425a
Family Computer <vinokhodova.nadin@gmail.com>
2022-11-04 20:32:47 +0100

    add task5

--- task5/script.js
+++ task5/script.js
@@ -1,34 +1,28 @@
 //Переписати код нижче з використанням конструкції switch…case
-// let numOrStr = prompt('input number or string');
-// if(numOrStr === null) {
-// console.log('ви скасували')
-// } else if( numOrStr.trim() === '' ) {
+//  let numOrStr = prompt('input number or string');
+//  if(numOrStr === null) {
+//  console.log('ви скасували')
+//  } else if( numOrStr.trim() === '' ) {
 // console.log('Empty String');
 // } else if ( isNaN( +numOrStr ) ) {
-// console.log(' number is Ba_NaN')
-// } else {
-// console.log('OK!')
-// }
+//  console.log(' number is Ba_NaN')
+//  } else {
+//  console.log('OK!')
+//  }
+
 
 
 numOrStr = prompt('input number or string');
-
 switch(numOrStr) {
     case null:
         console.log('ви скасували');
         break;
-    case '':
+    case '': 
         console.log('Empty String');
         break;
-    case NaN = true:
-        console.log('number is Ba_NaN');
+    case numOrStr:
+        console.log(' number is Ba_NaN');
         break;
     default:
-        console.log('OK!')
+        console.log('OK!');
 }
-
-
-
-
-
- 
